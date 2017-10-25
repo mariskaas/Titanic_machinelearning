008e1746692bae4ab131b1758f81c1303fd1ef97
Mariska <mwillemsen93@hotmail.com>
2017-10-25 11:14:49 +0200

    Finished building the predictor and predicting survival for the test set

--- Titanic_machine_learning.py
+++ Titanic_machine_learning.py
@@ -15,6 +15,7 @@ from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import linear_model
 
 print("Libraries have been imported")
 
@@ -44,8 +45,21 @@ b=titanic_training_data['Age']
 c=titanic_training_data['Fare']
 d=titanic_training_data['Survived']
 
-plt.scatter(b,c)
+plt.scatter(d,b)
 plt.show()
+
+#%%
+#Try to find out what we can fill age with if anything
+titanic_training_data.groupby(['Pclass'], as_index=False).mean()
+titanic_training_data.groupby(['Survived'], as_index=False).mean()
+titanic_training_data.groupby(['Sex'], as_index=False).mean()
+titanic_training_data.groupby(['Parch'], as_index=False).mean()
+
+#%%#Most reliable seems fill with class averages of age, did not matter for classifier but apparently kaggle wants me to fill it so fine.
+titanic_training_data.loc[(titanic_training_data['Age'].isnull()) & (titanic_training_data.Pclass == 1), 'Age']= 38.23
+titanic_training_data.loc[(titanic_training_data['Age'].isnull()) & (titanic_training_data.Pclass == 2), 'Age']= 29.87
+titanic_training_data.loc[(titanic_training_data['Age'].isnull()) & (titanic_training_data.Pclass == 3), 'Age']= 25.14
+print(titanic_training_data['Age'])
 
 #%%
 #There are two datapoints with an exceptionally high fare, may be typo so remove
@@ -53,9 +67,9 @@ titanic_training_data = titanic_training_data[titanic_training_data['Fare']<500]
 print("Outlier removed")
 #%%
 #Drop NaN values to make it workable for logarithm (subject to change according to features)
-titanic_training_data_drop = titanic_training_data[['Sex', 'Age', 'Survived', 'Fare']].dropna()
-print("Data before drop:", len(titanic_training_data['Age']))
-print("Data after drop:", len(titanic_training_data_drop['Age']))
+titanic_training_data_drop = titanic_training_data[['Sex', 'Age', 'Survived', 'Fare', 'Pclass', 'Parch']].dropna()
+print("Data before drop:", len(titanic_training_data['Sex']))
+print("Data after drop:", len(titanic_training_data_drop['Sex']))
 
 
 #%%
@@ -63,7 +77,7 @@ print("Data after drop:", len(titanic_training_data_drop['Age']))
 
 labels = titanic_training_data_drop['Survived']
 
-features = titanic_training_data_drop[['Sex', 'Age', 'Fare']]
+features = titanic_training_data_drop[['Sex', 'Fare', 'Pclass', 'Parch', 'Age']]
 x=features
 y=labels
 
@@ -142,7 +156,7 @@ print("F1 score mean for tree is:", scores_tree.mean())
 
 #%% #Trying Random forest Classifier
 #making the classififier
-clf_random_forest = RandomForestClassifier(class_weight = 'balanced')
+clf_random_forest = RandomForestClassifier(n_estimators=20, min_samples_split=9)
 
 #Fitting data and timing 
 t0=time()
@@ -178,3 +192,74 @@ print("F1 score mean for forest is:", scores_random_forest_fold_f1.mean())
 t0=time()
 print ("training time:", round(time()-t0, 3), "s")
 print("Accuracy score mean for forest is:", scores_random_forest_fold_accuracy.mean())
+
+#%%
+#Prepare the test set for predictions
+#Change male and female in 1 and 2
+titanic_test_data = titanic_test_data.replace('male', 0)
+titanic_test_data = titanic_test_data.replace('female', 1)
+print(titanic_test_data['Sex'])
+print("Males are 0, Females are 1")
+#%%
+#Make some plots to check for outliers
+a=titanic_test_data['Sex']
+b=titanic_test_data['Age']
+c=titanic_test_data['Fare']
+d=titanic_test_data['Pclass']
+plt.scatter(a,b)
+plt.show()
+plt.scatter(b,c)
+plt.show()
+plt.scatter(a,c)
+plt.show()
+plt.scatter(a,d)
+plt.show()
+plt.scatter(a,b)
+plt.show()
+plt.scatter(b,d)
+plt.show()
+plt.scatter(c,d)
+plt.show()
+#No crazy outliers found
+#%%
+#Fill age NaN because Kaggle specifically wants me to not drop them
+titanic_test_data.groupby(['Pclass'], as_index=False).mean()
+#Fill with average age per Pclass
+titanic_test_data.loc[(titanic_test_data['Age'].isnull()) & (titanic_test_data.Pclass == 1), 'Age']= 41
+titanic_test_data.loc[(titanic_test_data['Age'].isnull()) & (titanic_test_data.Pclass == 2), 'Age']= 29
+titanic_test_data.loc[(titanic_test_data['Age'].isnull()) & (titanic_test_data.Pclass == 3), 'Age']= 24
+print(titanic_test_data['Age'])
+#%%
+#Set the features from the test set
+
+features_test = titanic_test_data[['Sex', 'Age', 'Fare', 'Pclass', 'Parch']]
+print(np.isinf(titanic_test_data['Parch'].values.any()))
+print("Datatypes are:", titanic_test_data.dtypes)
+print(titanic_test_data.info())
+
+
+print("Test features have been set")
+#%% #Random forest classifier to predict from test set
+#making the classififier
+clf_random_forest_final = RandomForestClassifier(n_estimators=20, min_samples_split=9)
+
+#Fitting data and timing 
+t0=time()
+clf_random_forest_final.fit(x, y)
+print ("Training time:", round(time()-t0, 3), "s")
+
+#Predicing labels and timing
+t0=time()
+pred_random_forest_final = clf_random_forest_final.predict(features_test)
+print ("Predicting time:", round(time()-t0, 3), "s")
+
+y_pred = pred_random_forest_final
+
+print(len(y_pred))
+
+#%%
+#Making a dataframe with the passengderIDs and the list of predictions
+titanic_test_data_drop['Predictions'] = y_pred
+titanic_test_data_drop_newindex = titanic_test_data_drop.reset_index()
+titanic_predictions = titanic_test_data_drop_newindex[['PassengerId', 'Predictions']].copy()
+print(titanic_predictions)
